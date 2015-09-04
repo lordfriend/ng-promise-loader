b78e0c35fefa252b358a6fcba7c61c23d093e1c3
lordfriend <lordfriend@outlook.com>
2015-09-04 16:40:11 +0800

    add a test case

--- test/basic.spec.js
+++ test/basic.spec.js
@@ -16,7 +16,7 @@ describe('A promise-loader', function() {
   }));
 
   it('should show loader when promise is in pending state', function() {
-    var loadingContainer = $compile('<div class="loading-container" promise-loader="promise"></div>')($rootScope);
+    var loadingContainer = $compile('<div promise-loader="promise"></div>')($rootScope);
     $rootScope.$digest();
 
     // loader-backdrop is the container of loader element, which controls the visibility of loader.
@@ -37,5 +37,40 @@ describe('A promise-loader', function() {
     $rootScope.$digest();
 
     expect(loaderBackdrop.hasClass('show')).toBe(false);
-  })
+  });
+
+  it('should has show-backdrop class on loader-backdrop when backdrop=true', function () {
+    var loadingContainer = $compile('<div promise-loader="promise" backdrop="true"></div>')($rootScope);
+
+    $rootScope.$digest();
+
+    var loaderBackdrop = angular.element(loadingContainer[0].querySelector('.loader-backdrop'));
+
+    expect(loaderBackdrop.hasClass('show-backdrop')).toBe(true);
+  });
+
+  it('should show error toast when a promise is in rejected state', function() {
+    var ERROR_MESSAGE = 'error message';
+
+    var loadingContainer = $compile('<div promise-loader="promise"></div>')($rootScope);
+
+    $rootScope.$digest();
+
+    var errorBackdrop = angular.element(loadingContainer[0].querySelector('.error-backdrop'));
+    var errorText = angular.element(loadingContainer[0].querySelector('.error-text'));
+
+    var deferred = $q.defer();
+    $rootScope.promise = deferred.promise;
+
+    $rootScope.$digest();
+
+    expect(errorBackdrop.hasClass('show')).toBe(false);
+
+    deferred.reject(new Error(ERROR_MESSAGE));
+
+    $rootScope.$digest();
+
+    expect(errorBackdrop.hasClass('show')).toBe(true);
+    expect(errorText.text()).toEqual(ERROR_MESSAGE);
+  });
 });
